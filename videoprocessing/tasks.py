068cb52d75cacc8e7c4f489e9cea1c165e099215
shadowwa1k3r <ergash1994@gmail.com>
2022-05-08 10:35:11 +0500

    vh-host

--- videoprocessing/tasks.py
+++ videoprocessing/tasks.py
@@ -4,6 +4,6 @@ import requests
 
 @shared_task
 def create_task(id, name):
-    r = requests.post('http://192.168.0.104:8080/api/video/process/', json={"id": id, "name": name})
+    r = requests.post('http://54.201.176.199:8000/api/video/process/', json={"id": id, "name": name})
     print(r)
     return True
